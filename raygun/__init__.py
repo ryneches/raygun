e7e9fb0d1767a09570444f35866de5dd4080363a
Russell Neches <ryneches@ucdavis.edu>
2010-10-24 22:34:45 -0700

    added some docstrings, default values

--- raygun/__init__.py
+++ raygun/__init__.py
@@ -5,6 +5,8 @@ import shutil
 A simple BLAST wrapper. Use raygun to zap things.
 """
 
+DEFAULT_E = 0.001
+
 FIELDS = [ 'query', 'subject', 'percent_id', 'length', 'missmatches',
 'gaps', 'qstart', 'qend', 'sstart', 'send', 'e', 'bit' ]
 
@@ -22,7 +24,12 @@ class RayGun :
         cmd = 'cd ' + self.DIR + '; formatdb -p F -i ' + path + ' -n raygun'
         assert os.system( cmd ) == 0
 
-    def blastfile( self, infile, e ) :
+    def blastfile( self, infile, e = DEFAULT_E ) :
+        """
+        Run a BLAST search using an input file for the query sequence(s).  
+        Set your own e-value if you want. Returns a list of dictionaries 
+        for each hit.  
+        """
         path = os.path.abspath( infile )
         cmd = 'cd ' + self.DIR + '; blastall -p blastn -d raygun -i ' + path + ' -m 8 -e ' + str(e)
         m = os.popen( cmd )
@@ -36,9 +43,15 @@ class RayGun :
             hits.append( h )
         return hits
 
-    def blastseq( self, seq, e ) :
+    def blastseq( self, seq, queryname = 'query', e = DEFAULT_E ) :
+        """
+        Run a BLAST search using a string for the query sequence (i.e., 
+        only one sequence). Set your own e-value if you want. Returns
+        a list of dictionaries for each hit. The query sequence
+        identifier will be 'query' unless you set queryname.
+        """
         q = self.DIR + '/query.fa'
-        open( q, 'w' ).write( '>query\n' + str(seq) )
+        open( q, 'w' ).write( '>' + queryname +'\n' + str(seq) )
         return self.blastfile( q, e )
             
     def __del__( self ) :
